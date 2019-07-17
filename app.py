5b756d082ea738b46f3266f493440d72f064d6dc
webacer <654994961@qq.com>
2019-07-17 11:27:01 +0800

    Initial commit
    Signed-off-by: webacer <654994961@qq.com>

--- app.py
+++ app.py
@@ -11,7 +11,20 @@ def hello_world():
 @app.route('/hello')  # 为视图绑定多个url
 @app.route('/hi')
 def say_hello():
-    return '<h1>Hello, Flask!<h1>'
+    return '<h1>Hello, Flask!</h1>'
+
+
+@app.route('/greet', defaults={'name': 'Programmer'})  # 设置默认值
+@app.route('/greet/<name>')  # 动态url
+def greet1(name):
+    return '<h1>Hello, %s!</h1>' % name
+
+
+# 等同于
+@app.route('/greet')
+@app.route('/greet/<name>')
+def greet2(name='Programmer'):
+    return '<h1>Hello, %s!</h1>' % name
 
 
 if __name__ == '__main__':
