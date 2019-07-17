1dd971d7ef84d83363c1c3148671e91ae4fa27b5
webacer <654994961@qq.com>
2019-07-17 15:15:23 +0800

    Initial commit
    Signed-off-by: webacer <654994961@qq.com>

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -17,14 +17,29 @@ def say_hello():
 @app.route('/greet', defaults={'name': 'Programmer'})  # 设置默认值
 @app.route('/greet/<name>')  # 动态url
 def greet1(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return '<h1>Hello, %s is pig !</h1>' % name
 
 
 # 等同于
 @app.route('/greet')
 @app.route('/greet/<name>')
 def greet2(name='Programmer'):
+    return '<h1>Hello, %s !</h1>' % name
+
+
+@app.route('/hello1', methods=['GET'])
+def hello():
+    name = request.args.get('name', 'Flask')
+    print(request.args)  # 解析后的数据
+    print(request.query_string)  # 原始查询语句
     return '<h1>Hello, %s!</h1>' % name
+
+
+@app.route('/colors/<any(blue, white, red):color>')  # any转换器
+def three_colors(color):
+    colors = ['blue', 'white', 'red']
+    print(str(colors)[1:-1])
+    return '<p> Love is patient and kind. Love is not jealous or boastful or proud or rude.</p>'
 
 
 if __name__ == '__main__':
